7f58f6ab0dd151db49d7c41795c3471e663691bf
NAME <mail@gmail.com>
2024-02-25 23:38:56 +0300

    + update readme

--- TextGen.py
+++ TextGen.py
@@ -1,7 +1,11 @@
 import langchain_community
 from langchain_community.llms import KoboldApiLLM
-
-llm = langchain_community.llms.koboldai.KoboldApiLLM(endpoint="https://imagine-saddam-hero-clinic.trycloudflare.com/api", max_length=666)
+endpoint1=""
+print("Paste the link: ")
+endpoint1=input()
+endpoint1=endpoint1+"api"
+print(endpoint1)
+llm = langchain_community.llms.koboldai.KoboldApiLLM(endpoint=endpoint1, max_length=666)
 quest=""
 quest=input()
 response = llm(quest) 
