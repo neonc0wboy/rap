b89b67934190da1dad8c05f3bd36e5eeaf3cc699
c0wb0y <nikita.mailml@gmail.com>
2024-03-13 10:17:31 +0300

    improve

--- scr.sh
+++ scr.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Generate a new seed using the RapSeedBIP39 script
 seed=$(./RapSeedBIP39)
-echo $seed > key.tmp
-echo $seed >> seeds 
-num=$(tail -n 1 ./wnum)
 
-cli-wallet --create-from-key "$(tail -n 1 key.tmp)" --witness-type legacy w[i]
-cli-wallet --create-from-key "$(tail -n 1 key.tmp)" --witness-type segwit w[i+1]
+# Save the seed to a temporary file
+echo $seed > key.tmp
+
+# Append the seed to the seeds file
+echo $seed >> seeds
+
+# Get the last line number from the wnum file
+num=$(wc -l < ./wnum)
+
+# Create two new wallets with different witness types using the generated seed
+./cli-wallet --create-from-key "$(tail -n 1 key.tmp)" --witness-type legacy --file w[${num}]
+./cli-wallet --create-from-key "$(tail -n 1 key.tmp)" --witness-type segwit --file w[${num}].segwit
+
+# Remove the temporary file
+rm key.tmp
